bc98ee3c60d8075902b7cacd371ad317fab74783
yaboiiclancy <yaboiiclancy@gmail.com>
2021-06-15 16:19:33 -0700

    Create Solarsystem.js

---
+++ Solarsystem.js
@@ -0,0 +1,68 @@
+var SolarSystem = {
+  Sun : {
+    radius : 3 * 3,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 233/255, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 0.0553 * 3,
+    distance : 0.387,
+    year : 0.241,
+    color : [ 177/255, 104/255, 84/255, 1.0 ]
+  },
+  Venus : {
+    radius : 0.949,
+    distance : 0.723,
+    year : 0.615,
+    color : [ 169/255, 165/255, 40/255, 1.0 ]
+  },
+  Earth : {
+    radius : 1,
+    distance : 1,
+    year : 1,
+    color : [ 3/255, 145/255, 79/255, 1.0 ]
+  },
+  Moon : {
+    radius : 0.2724,
+    distance : 0.00257,
+    year : 0.0748,
+    color : [ 169/255, 175/255, 182/255, 1.0 ]
+  },
+  Mars : {
+    radius : 0.532,
+    distance : 1.52,
+    year : 1.88,
+    color : [ 194/255, 39/255, 25/255, 1.0 ]
+  },
+  Jupiter : {
+    radius : 11.21 / 3,
+    distance : 5.20,
+    year : 11.9,
+    color : [ 213/255, 144/255, 74/255, 1.0 ]
+  },
+  Saturn : {
+    radius : 9.45 / 3,
+    distance : 9.58,
+    year : 29.4,
+    color : [ 236/255, 193/255, 118/255, 1.0 ]
+  },
+  Uranus : {
+    radius : 4.01 / 3,
+    distance : 19.20,
+    year : 83.7,
+    color : [ 18/255, 187/255, 240/255, 1.0 ]
+  },
+  Neptune : {
+    radius : 3.88 / 3,
+    distance : 30.05,
+    year : 163.7,
+    color : [ 33/255, 93/255, 241/255, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.186,
+    distance : 39.48,
+    year : 247.9,
+    color : [ 186/255, 153/255, 126/255, 1.0 ]
+  }
+};
