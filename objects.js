48886a0062e2aba3e4fa8b2dbfd238564b19ee70
yaboiiclancy <yaboiiclancy@gmail.com>
2021-06-29 16:43:20 -0700

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,96 @@
+var img = 'midterm-bg.png';
+
+var Spheres = {
+    Tire : {
+        model : undefined,
+        radius : 12,  
+        distance : 0,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+
+    Wheel : {
+        model : undefined,
+        radius : 8,
+        distance : 12,
+        color : [ 0.0, 0.0, 0.0, 1.0 ]
+    },
+
+    Center : {
+        model : undefined,
+        radius : 2,
+        distance : 20,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+};
+
+var Cylinders = {
+    Spoke1 : {
+        model : undefined,
+        radius : 0.4,
+        distance : 4,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+
+    Spoke2 : {
+        model : undefined,
+        radius : 0.4,
+        distance : 4,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+
+    Spoke3 : {
+        model : undefined,
+        radius : 0.4,
+        distance : 4,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+
+    Spoke4 : {
+        model : undefined,
+        radius : 0.4,
+        distance : 4,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+
+    Spoke5 : {
+        model : undefined,
+        radius : 0.4,
+        distance : 4,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+
+    Spoke6 : {
+        model : undefined,
+        radius : 0.4,
+        distance : 4,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+
+    Spoke7 : {
+        model : undefined,
+        radius : 0.4,
+        distance : 4,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    },
+
+    Spoke8 : {
+        model : undefined,
+        radius : 0.4,
+        distance : 4,
+        color : [ 105/255, 105/255, 105/255, 1.0 ]
+    }
+};
+
+var BGs = {
+    Background1 : {
+        model : undefined,
+        distance : 10,
+        source : img,
+    },
+
+    Background2 : {
+        model : undefined,
+        distance : 10,
+        source : img,
+    }
+};
